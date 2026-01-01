8b7a81b91041770f2bbb3ce1e9fd612e2a766f17
Bianca Soares <blancasuarz@gmail.com>
2026-01-01 16:34:51 -0300

    Explorando frames

--- SimpleFrameTest.java
+++ SimpleFrameTest.java
@@ -16,8 +16,13 @@ public class SimpleFrameTest {
 class SimpleFrame extends JFrame {
     public SimpleFrame() {
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        setLocation(600, 200);
+        setTitle("Bianca Soares");
     }
 
     public static final int DEFAULT_WIDTH = 600;
     public static final int DEFAULT_HEIGHT = 600;
+    public static final int DEFAULT_X = 600;
+    public static final int DEFAULT_Y = 200;
+    public static final String DEFAULT_TITLE = "Bianca Soares";
 }
