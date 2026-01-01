e5313f3ec902d2425b4d4138183d3c2066c0599e
Bianca Soares <blancasuarz@gmail.com>
2026-01-01 17:04:28 -0300

    Explorando propriedades do Frame

--- SizedFrameTest.java
+++ SizedFrameTest.java
@@ -1,43 +1,43 @@
-import java.awt.EventQueue;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class SimpleFrameTest {
+public class SizedFrameTest {
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                SimpleFrame frame = new SimpleFrame();
+                SizedFrame frame = new SizedFrame();
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setVisible(true);
-
-                Toolkit kit = Toolkit.getDefaultToolkit();
-                Dimension screenSize = kit.getScreenSize();
-                int screenWidth = screenSize.width;
-                int screenHeight = screenSize.height;
             }
         });
-
     }
 }
 
-class SimpleFrame extends JFrame {
-    public SimpleFrame() {
-        //setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        //setLocation(600, 200);
-        setTitle("Bianca Soares");
+class SizedFrame extends JFrame {
+    public SizedFrame() {
+
+        // Obtém as dimensões da tela
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+
+        // Configura a largura e altura do frame e deixa a plataforma decidir a localização
         setBounds(DEFAULT_X, DEFAULT_Y, DEFAULT_WIDTH, DEFAULT_HEIGHT);
         setSize(screenWidth / 2, screenHeight / 2);
         setLocationByPlatform(true);
+
+        // Configura o título e o ícone do frame
+        setTitle("Bianca Soares");
         Image img = kit.getImage("icon.png");
         setIconImage(img);
     }
 
     public static final int DEFAULT_WIDTH = 600;
     public static final int DEFAULT_HEIGHT = 600;
-    //public static final int DEFAULT_X = 600;
-    //public static final int DEFAULT_Y = 200;
+    public static final int DEFAULT_X = 600;
+    public static final int DEFAULT_Y = 200;
     public static final String DEFAULT_TITLE = "Bianca Soares";
     public static final int DEFAULT_BOUNDS_X = 600;
     public static final int DEFAULT_BOUNDS_Y = 200; 
-    public static final int DEFAULT_X = 600;
-    public static final int DEFAULT_Y = 200;
 }
